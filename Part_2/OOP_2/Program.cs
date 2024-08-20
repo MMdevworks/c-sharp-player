89c0a60a7f8ee13dee664f426a40225ed3e0021c
Marie Marcos <mmdevq@gmail.com>
2024-08-20 11:57:29 -0700

    class challenge

--- Part_2/OOP_2/Program.cs
+++ Part_2/OOP_2/Program.cs
@@ -3,36 +3,35 @@
 // Object-Oriented Principle #1: Encapsulation — Combining data (fields) and the operations on that data (methods) into a well-defined unit (like a class).
 
 
-Arrow make = new Arrow("aer", "agr", 1);
+Arrow make = new Arrow();
+// Console.WriteLine(make._arrowhead);
+// Console.WriteLine(make._fletching);
+// Console.WriteLine(make._length);
+
+Console.Write("What type of arrowhead? ");
+make._arrowhead = Console.ReadLine().ToLower();
+// Arrow.GetArrowHead(make._arrowhead);
+
 Console.WriteLine(make._arrowhead);
-Console.WriteLine(make._fletching);
-Console.WriteLine(make._length);
 
 
 class Arrow {
-    public string _arrowhead;
-    public string _fletching;
-    public int _length;
+    public ArrowHead _arrowhead;
+    public Fletching _fletching;
+    public float _length;
 
-    public Arrow(string arrowhead, string fletching, int length) {
+    public Arrow(ArrowHead, Fletching, float length) {
         _arrowhead = arrowhead;
         _fletching = fletching;
         _length = length;
     }
 
-    // public ArrowHead GetArrowHead(){
-    //     Console.Write("What type of arrowhead? ");
-    //     string userInput = Console.ReadLine().ToLower();
-    //     ArrowHead selected = userInput switch{
-    //         "steel" => ArrowHead.Steel,
-    //         "wood" => ArrowHead.Wood,
-    //         "obsidian" => ArrowHead.Obsidian,
-    //     };
-    // }
-
-    enum ArrowHead { Steel, Wood, Obsidian};
-    // enum fletching { Plastic, Turkey, Goose};
 }
+    enum ArrowHead { Steel, Wood, Obsidian};
+    enum Fletching { Plastic, Turkey, Goose};
+
+
+
 
 // Score best = new Score();
 // best.name = "M";
