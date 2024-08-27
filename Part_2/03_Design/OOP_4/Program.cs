4d129f5db5c51850604f00a82e9875a327e55d27
Marie Marcos <mmdevq@gmail.com>
2024-08-27 14:14:40 -0700

    object oriented design

---
+++ Part_2/03_Design/OOP_4/Program.cs
@@ -0,0 +1,75 @@
+﻿// ------ Object Oriented Design -------
+
+// ---- Rock Paper Scissors ----
+
+// Requirements
+// • Two human players compete against each other.
+// • Each player *picks Rock, Paper, or Scissors.
+// • Depending on the players’ choices, a winner is *determined: Rock beats Scissors, Scissors beats Paper, Paper beats Rock. If both players pick the same option, it is a draw.
+// • The game must *display who won the round.
+// • The game will keep *running rounds until the window is closed but must *remember the historical
+//   record of how many times each player won and how many draws there were.
+
+
+// Objects (instance of a class)
+    // Player 1
+    // Player 2
+    // RPS game
+
+// Classes
+    // -class Player-
+        // • Knows the name of the players
+        // • Sets player choice
+        // • Knows players Score? - (-Score-)
+    // -class RPSGame-
+        // • Knows game logic and win/end conditions
+        // • Knows what players exist - (-Player-)
+        // • Updates the game each round
+        // • Display Result
+    // -class ScoreRecord-
+        // • Keeps track of round winner
+
+// ---- 15 Puzzle ----
+
+// Requirements
+// • The player needs to be able to *manipulate the board to rearrange it.
+// • The current state of the game needs to be *displayed to the user.
+// • The game needs to *detect when it has been solved and tell the player they won.
+// • The game needs to be able to *generate random puzzles to solve.
+// • The game needs to *track and *display how many moves the player has made.
+
+// Objects
+    
+// Classes
+    // -class Player-
+        // • Track and display player moves
+        // •
+
+    // -class GameBoard-
+        // • Generate random puzzle
+        // • Display state of game board
+        // • Know game end condition and tell the player win/lose - (-Player-)
+
+
+
+
+// ---- Hangman ----
+
+// Requirements
+
+// Objects
+    
+// Classes
+    // -class -
+        // •
+
+
+// ---- Tic Tac Toe ----
+
+// Requirements
+
+// Objects
+    
+// Classes
+    // -class -
+        // •
