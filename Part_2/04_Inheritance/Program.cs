c711505e9fae083bd158c93fb81da7646fdaf7e4
Marie Marcos <mmdevq@gmail.com>
2024-09-03 11:56:28 -0700

    inheritance item class

---
+++ Part_2/04_Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+class InventoryItem {
+    public float Weight;
+    public float Volume;
+
+    public InventoryItem(float weight, float volume) {
+        Weight = weight;
+        Volume = volume;
+    }
+}
