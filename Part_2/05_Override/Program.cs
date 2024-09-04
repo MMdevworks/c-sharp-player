3a99ac7119642c89eb236832cb1b23b1cf7f623e
Marie Marcos <mmdevq@gmail.com>
2024-09-04 11:08:07 -0700

    override ex

---
+++ Part_2/05_Override/Program.cs
@@ -0,0 +1,16 @@
+﻿// Example use of 'override' modifier. 
+// Override is used to provide a new implementation for a method that is already defined in a base class
+// In this example ToString is method defined in the object base class - the ultimate parent of all classes in C#
+
+public class InventoryItem {
+    public string Name { get; }
+    public float Weight { get; }
+    public float Volume { get; }
+
+    public InventoryItem(string name, float weight, float volume) 
+    {   
+        Name = name;
+        Weight = weight;
+        Volume = volume;
+    }
+}
