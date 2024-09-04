d90ef7426729e0473ad0bd7e50d41bac57b1b528
Marie Marcos <mmdevq@gmail.com>
2024-09-04 11:30:24 -0700

    rbt cmd classes

--- Part_2/05_Polymorphism/05_PolyRobot/Program.cs
+++ Part_2/05_Polymorphism/05_PolyRobot/Program.cs
@@ -1,13 +1,39 @@
 ﻿public class Robot
 {
-public int X { get; set; }
-public int Y { get; set; }
-public bool IsPowered { get; set; }
-public RobotCommand?[] Commands { get; } = new RobotCommand?[3]; public void Run()
-{
-foreach (RobotCommand? command in Commands)
-{
-command?.Run(this);
-Console.WriteLine($"[{X} {Y} {IsPowered}]"); }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public RobotCommand?[] Commands { get; } = new RobotCommand?[3]; public void Run()
+    {
+        foreach (RobotCommand? command in Commands)
+        {
+        command?.Run(this);
+        Console.WriteLine($"[{X} {Y} {IsPowered}]"); }
+    }
 }
+
+public class RobotCommand
+{
+    public abstract void Run(Robot robot)
+    {
+
+    } 
+}
+
+public class OnCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (!robot.IsPowered)
+            robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+            robot.IsPowered = false;
+    }
 }
