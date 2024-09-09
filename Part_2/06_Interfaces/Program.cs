8ed3c63231d2f60a269bb5c706db0090a49b42c8
Marie Marcos <mmdevq@gmail.com>
2024-09-09 07:14:56 -0700

    interfaces

--- Part_2/06_Interfaces/Program.cs
+++ Part_2/06_Interfaces/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Enter 3 Commands: (on, off, north, south, east, west)");
 for (int i = 0; i < robot.Commands.Length; i++)
 {
     string commands = Console.ReadLine().ToLower();
-    RobotCommand newCommand = commands switch 
+    IRobotCommand newCommand = commands switch 
     {
         "on" => new OnCommand(),
         "off" => new OffCommand(),
@@ -18,39 +18,39 @@ for (int i = 0; i < robot.Commands.Length; i++)
 
 robot.Run();
 
-public class OnCommand : RobotCommand
+public class OnCommand : IRobotCommand
 {
-    public override void Run(Robot robot) => robot.IsPowered = true;
+    public void Run(Robot robot) => robot.IsPowered = true;
 }
 
-public class OffCommand : RobotCommand
+public class OffCommand : IRobotCommand
 {
-    public override void Run(Robot robot) => robot.IsPowered = false;
+    public void Run(Robot robot) => robot.IsPowered = false;
 }
 
-public class NorthCommand : RobotCommand
+public class NorthCommand : IRobotCommand
 {
-    public override void Run(Robot robot) { if (robot.IsPowered) robot.Y += 1; }
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y += 1; }
 }
 
-public class SouthCommand : RobotCommand
+public class SouthCommand : IRobotCommand
 {
-    public override void Run(Robot robot) { if (robot.IsPowered) robot.Y -= 1; }
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y -= 1; }
 }
 
-public class EastCommand : RobotCommand
+public class EastCommand : IRobotCommand
 {
-    public override void Run(Robot robot) { if (robot.IsPowered) robot.X += 1; }
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X += 1; }
 }
 
-public class WestCommand : RobotCommand
+public class WestCommand : IRobotCommand
 {
-    public override void Run(Robot robot) { if (robot.IsPowered) robot.X -= 1; }
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X -= 1; }
 }
 
-public abstract class RobotCommand
+    public interface IRobotCommand
 {
-       public abstract void Run(Robot robot);
+       void Run(Robot robot);
 }
 
 public class Robot
@@ -58,9 +58,9 @@ public class Robot
     public int X { get; set; }
     public int Y { get; set; }
     public bool IsPowered { get; set; }
-    public RobotCommand?[] Commands { get; } = new RobotCommand?[3]; public void Run()
+    public IRobotCommand?[] Commands { get; } = new IRobotCommand?[3]; public void Run()
     {
-        foreach (RobotCommand? command in Commands)
+        foreach (IRobotCommand? command in Commands)
         {
         command?.Run(this);
         Console.WriteLine($"[{X} {Y} {IsPowered}]"); }
