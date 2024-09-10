d0002e1aba4a4724986585fa9adb942c6a7fc47b
Marie Marcos <mmdevq@gmail.com>
2024-09-10 16:17:13 -0700

    mdev

---
+++ Part_2/08_Records/Program.cs
@@ -0,0 +1,5 @@
+﻿// Records are a compact alternative notation for defining a data-centric class or struct: public record Point(float X, float Y);
+// The compiler automatically generates a constructor, properties, ToString, equality with value semantics, and deconstruction.
+// Records are turned into classes by default or into a struct (public record struct Point(...)).
+
+
