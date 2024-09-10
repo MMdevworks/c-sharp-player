f477e6df9e6eb92e0d7c8616e87818b062e0ade0
Marie Marcos <mmdevq@gmail.com>
2024-09-09 20:15:52 -0700

    struct

--- Part_2/07_Structs/Program.cs
+++ Part_2/07_Structs/Program.cs
@@ -1,29 +1,38 @@
 ﻿// A struct is a custom value type, short for structure or data structure.  Structs are not focused on behavior but can have properties and methods.
 // Structs are nearly the same as using class except structs are value types instead of reference types.
 
+Coordinate coord1 = new Coordinate(1,1);
+Coordinate coord2 = new Coordinate(2,1);
+Coordinate coord3 = new Coordinate(4,4);
+Coordinate coord4 = new Coordinate(-3, -2);
+Coordinate coord5 = new Coordinate(-2, -2);
 
 
-// Create a Coordinate struct that can represent a room coordinate with a row and column. Sold to
-// • Ensure Coordinate is immutable.
-// • Make a method to determine if one coordinate is adjacent to another (differing only by a single row or column).
-// • Write a main method that creates a few coordinates and determines if they are adjacent to each other to prove that it is working correctly.
+Console.WriteLine(Coordinate.AreAdjacent(coord1, coord2));
+Console.WriteLine(Coordinate.AreAdjacent(coord1, coord3));
+Console.WriteLine(Coordinate.AreAdjacent(coord2, coord3));
+Console.WriteLine(Coordinate.AreAdjacent(coord4, coord5));
 
-CoordinateStruct coord1 = new CoordinateStruct(1,1);
-CoordinateStruct coord2 = new CoordinateStruct(2,1);
-CoordinateStruct coord3 = new CoordinateStruct(4,4);
-
-public struct CoordinateStruct
+public struct Coordinate
 {
-    public int row {get;}
-    public int col {get;}
+    public int Row {get;}
+    public int Col {get;}
 
+    public Coordinate(int row, int col)
+    {
+        Row = row;
+        Col = col;
+    }
+
+public static bool AreAdjacent(Coordinate coord1, Coordinate coord2)
+    {
+        int rowDiff = coord1.Row - coord2.Row;
+        int colDiff = coord1.Col - coord2.Col;
+        
+        if (rowDiff <= 1 && colDiff == 0) return true;
+        if (colDiff <= 1 && rowDiff == 0) return true;
+
+        return false;
+    }
 }
 
-
-// {
-
-// if(coord)
-//     row -= 1 || row += 1;
-//     col -= 1 || col += 1;
-    
-// }
