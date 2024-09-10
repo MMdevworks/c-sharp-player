2667a9ac39205380aaba55970fdb6c2a1a5d7cb3
Marie Marcos <mmdevq@gmail.com>
2024-09-10 16:23:27 -0700

    mdev

--- Part_2/08_Records/Program.cs
+++ Part_2/08_Records/Program.cs
@@ -2,4 +2,7 @@
 // The compiler automatically generates a constructor, properties, ToString, equality with value semantics, and deconstruction.
 // Records are turned into classes by default or into a struct (public record struct Point(...)).
 
+public record Sword(Material material, Gemstone gemstone, Length length, CrossguardWidth crossguardWidth);
 
+enum Material { Wood, Bronze, Iron, Steel, Binarium };
+enum Gemstone { Emerald, Amber, Sapphire, Diamond, Bitstone, Bare };
