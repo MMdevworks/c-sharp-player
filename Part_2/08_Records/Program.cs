a7e4146e35cb7337fbcd4bba6625a2be804de6d7
Marie Marcos <mmdevq@gmail.com>
2024-09-10 17:03:02 -0700

    mdev

--- Part_2/08_Records/Program.cs
+++ Part_2/08_Records/Program.cs
@@ -2,7 +2,15 @@
 // The compiler automatically generates a constructor, properties, ToString, equality with value semantics, and deconstruction.
 // Records are turned into classes by default or into a struct (public record struct Point(...)).
 
-public record Sword(Material material, Gemstone gemstone, Length length, CrossguardWidth crossguardWidth);
+Sword basic = new Sword(Material.Iron, Gemstone.Bare, 36, 5.5f);
+// using record and the 'with' expression
+Sword basicFire = basic with { gemstone = Gemstone.Ruby };
+Sword basicIceDagger = basic with { gemstone = Gemstone.Sapphire, length = 7, crossguardWidth = 2 };
 
-enum Material { Wood, Bronze, Iron, Steel, Binarium };
-enum Gemstone { Emerald, Amber, Sapphire, Diamond, Bitstone, Bare };
+Console.WriteLine(basic);
+Console.WriteLine(basicFire);
+Console.WriteLine(basicIceDagger);
+
+public record Sword(Material material, Gemstone gemstone, float length, float crossguardWidth);
+public enum Material { Wood, Bronze, Iron, Steel, Adamantium };
+public enum Gemstone { Emerald, Amber, Sapphire, Diamond, Ruby, Bare };
